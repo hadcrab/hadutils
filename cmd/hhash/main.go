b3207fae5e2263ab9c22784b42d788181effba46
Mihail <mihailsorin58@gmail.com>
2026-08-06 13:50:29 +0500

    feat: readFile test implementation

--- cmd/hhash/main.go
+++ cmd/hhash/main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"io"
+	"os"
 	"fmt"	
 	// "github.com/hadcrab/hadutils/internal/hash"
 )
 
+func readFile(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+	
+	defer file.Close()
+	
+	return string(data), nil
+} 
+
 func main() {
-	fmt.Printf("Hello test hi")
+	file, err := readFile("testfile.txt")
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	fileTest, err := os.ReadFile("testfile.txt")
+	
+	fmt.Printf(file)
+	fmt.Printf(string(fileTest))
 } 
