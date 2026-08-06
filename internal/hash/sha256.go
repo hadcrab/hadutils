c17cc2d629fc21139079300808a3a3749469cd96
Mihail <mihailsorin58@gmail.com>
2026-08-06 15:31:40 +0500

    feat: sha256 encoding logic

--- internal/hash/sha256.go
+++ internal/hash/sha256.go
@@ -1,6 +1,30 @@
 package hash
 
-// import (
-// 	"crypto/sha256"
-// )
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"os"
+	"fmt"
+)
 
+type Algorithm string
+
+const (
+    SHA256 Algorithm = "sha256"
+)
+
+func Compute(path string, algorithm Algorithm) (string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	switch algorithm {
+		case SHA256:
+			sum := sha256.Sum256(data)
+			hash := hex.EncodeToString(sum[:])
+			return hash, nil
+		default:
+    		return "", fmt.Errorf("unsupported algorithm: %s", algorithm)
+	}
+}
