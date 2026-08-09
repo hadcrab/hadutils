519d5f9600ea1c23b4dc9b2f19ea2a4e12b6ebc2
Mihail <mihailsorin58@gmail.com>
2026-08-09 16:50:26 +0500

    feat: add support for sha1, sha512 and md5 algorithms

--- internal/hash/hash.go
+++ internal/hash/hash.go
@@ -2,6 +2,9 @@ package hash
 
 import (
 	"crypto/sha256"
+	"crypto/sha512"
+	"crypto/sha1"
+	"crypto/md5"
 	"encoding/hex"
 	"os"
 	"fmt"
@@ -12,6 +15,9 @@ type Algorithm string
 const (
 	UnknownAlgorithm Algorithm = ""
     SHA256 Algorithm = "sha256"
+    SHA512 Algorithm = "sha512"
+    SHA1 Algorithm = "sha1"
+    MD5 Algorithm = "md5"
 )
 
 func Compute(path string, algorithm Algorithm) (string, error) {
@@ -19,12 +25,19 @@ func Compute(path string, algorithm Algorithm) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
 	switch algorithm {
 		case SHA256:
 			sum := sha256.Sum256(data)
-			hash := hex.EncodeToString(sum[:])
-			return hash, nil
+			return hex.EncodeToString(sum[:]), nil
+		case SHA512:
+			sum := sha512.Sum512(data)
+			return hex.EncodeToString(sum[:]), nil
+		case SHA1:
+			sum := sha1.Sum(data)
+			return hex.EncodeToString(sum[:]), nil
+		case MD5:
+			sum := md5.Sum(data)
+			return hex.EncodeToString(sum[:]), nil
 		default:
     		return "", fmt.Errorf("unsupported algorithm: %s", algorithm)
 	}
