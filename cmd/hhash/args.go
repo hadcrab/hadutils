70c94fd7398f99508973102c39ebdd9f41d6de28
Mihail <mihailsorin58@gmail.com>
2026-08-19 13:59:46 +0500

    refactor(hhash): use flag.StringVar instead of flag.String, add constant for default hash algo

--- cmd/hhash/args.go
+++ cmd/hhash/args.go
@@ -7,22 +7,32 @@ import (
 	"github.com/hadcrab/hadutils/internal/hash"
 )
 
+const defaultAlgorithm = "SHA256" 
 
 func collectArgs() (Config, error) {
-	algo := flag.String("a", "SHA256", "Algorithm")
-	quiet := flag.Bool("q", false, "Print only hash")
-	copyToClipboard := flag.Bool("c", false, "Quietly copy hash to clipboard")
+	var (
+		algo string
+		quiet bool
+		copyToClipboard bool
+	)
+	flag.StringVar(&algo, "a", defaultAlgorithm, "Algorithm")
+	flag.StringVar(&algo, "algorithm", defaultAlgorithm, "Algorithm")
+	flag.BoolVar(&quiet, "q", false, "Print only hash")
+	flag.BoolVar(&quiet, "quiet", false, "Print only hash")
+	flag.BoolVar(&copyToClipboard, "c", false, "Quietly copy hash to clipboard")
+	flag.BoolVar(&copyToClipboard, "copy", false, "Quietly copy hash to clipboard")
 	flag.Parse()
-	algorithm := hash.Algorithm(strings.ToLower(*algo))
+	algorithm := hash.Algorithm(strings.ToLower(algo))
 	args := flag.Args()
+	
 	if len(args) != 1 {
     	return Config{}, errors.New("exactly one file path is required")
 	}
 	cfg := Config{
 		Path: args[0],
 		Algorithm: algorithm,
-		Quiet: *quiet,
-		CopyToClipboard: *copyToClipboard,
+		Quiet: quiet,
+		CopyToClipboard: copyToClipboard,
 	}
 	
 	return cfg, nil
