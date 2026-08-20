82b261d97a77dc6b8a2b5977137a4e43f10a83e3
Mihail <mihailsorin58@gmail.com>
2026-08-20 16:01:26 +0500

    feat: return -c flag

--- cmd/hhash/run.go
+++ cmd/hhash/run.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	//"os"
 	"github.com/hadcrab/hadutils/internal/hash"
-	// "github.com/hadcrab/hadutils/internal/env"
-	// "github.com/hadcrab/hadutils/internal/clipboard"
+	"github.com/hadcrab/hadutils/internal/env"
+	"github.com/hadcrab/hadutils/internal/clipboard"
 )
 
 func run() error {
-	// env := env.Collect()
+	env := env.Collect()
 	cfg, err := collectArgs()
 	if err != nil {
 		return err
 	}
-
+	
 	for _, path := range cfg.Path {
 		sum, err := hash.Compute(path, cfg.Algorithm)
 		if err != nil {
 			return err
 		}
 		if cfg.CopyToClipboard {
-			//todo add support for this
-			return errors.New("cannot use --copy with multiple files ")
+			clipboard.CopyIn(sum, env.Clipboard)
+			// todo: make it work with multiple files
+			return nil
     	}
 		if cfg.Quiet {
 			fmt.Println(sum)
