07a2bbf39d89258a963ad0a39dbcd579b51a0819
Mihail <mihailsorin58@gmail.com>
2026-08-25 15:15:35 +0500

    feat(cli): create API structure, add parse func

---
+++ cli/parse.go
@@ -0,0 +1,37 @@
+package cli
+
+import (
+	"slices"
+	"strings"
+	"errors"
+)
+
+func Parse(argv []string, defs ...*Argument) ([]string, error) {
+	var positionals []string
+	for i := 0; i < len(argv); i++ {
+		token := argv[i]
+		if !strings.HasPrefix(token, "-") {
+			positionals = append(positionals, token)
+			continue
+		}
+		name := strings.TrimLeft(token, "-")
+		var found *Argument
+		for argIndex := range defs {
+			if slices.Contains(defs[argIndex].names, name) {
+					found = defs[argIndex]
+				}
+			if found != nil {
+				break
+			}
+		}
+		if found == nil {
+			return nil, errors.New("unknown argument: " + token)
+		}
+		consumed, err := found.behavior.Parse(argv[i+1:], found.TakeValue)
+		if err != nil {
+			return nil, err
+		}
+		i += consumed
+	}
+	return positionals, nil
+}
--- cli/structs.go
+++ cli/structs.go
@@ -1,6 +1,7 @@
-package main
+package cli
 
 import (
+	"errors"
 	"strconv"
 )
 
@@ -59,35 +60,83 @@ func (v *BoolValue) Set(value string) error {
 }
 
 type Behavior interface {
-	Parse(args []string) (consumed int, err error)
+	Parse(args []string, set func(string) error) (consumed int, err error)
 }
 
-type Flag struct {
-	Value bool
+type Flag struct{}
+
+func (f Flag) Parse(_ []string, set func(string) error) (int, error) {
+	return 0, set("true")
 }
 
-func (f *Flag) Parse(args []string) (int, error) {
-	f.Value = true
-	return 0, nil
-}
+type Option struct{}
 
-type Option struct {
-	Value Value
-}
-
-func (o *Option) Parse(args []string) (int, error) {
+func (o Option) Parse(args []string, set func(string) error) (int, error) {
 	if len(args) == 0 {
-		return 0, errors.New("option requires a value")
+		return 0, errors.New("requires a value")
 	}
-	if err := o.Value.Set(args[0]); err != nil {
-		return 0, err
-	}
-	return 1, nil
+	return 1, set(args[0])
 }
 
 type Argument struct {
-	Names       []string
-	Description string
-	Required    bool
-	Behavior    Behavior
+	names       []string
+	description string
+	required    bool
+
+	deflt      string
+	hasDefault bool
+	value      Value
+	behavior   Behavior
+}
+
+func String(names ...string) *Argument {
+	return argument(&StringValue{}, &Option{}, names...)
+}
+
+func Int(names ...string) *Argument {
+	return argument(&IntValue{}, &Option{}, names...)
+}
+
+func Bool(names ...string) *Argument {
+	return argument(&BoolValue{}, &Flag{}, names...)
+}
+
+func argument(value Value, behavior Behavior, names ...string) *Argument {
+	return &Argument{
+		names:    names,
+		value:    value,
+		behavior: behavior,
+	}
+}
+
+func (a *Argument) TakeValue(val string) error {
+	return a.value.Set(val)
+}
+
+func (a *Argument) Description(description string) *Argument {
+	a.description = description
+	return a
+}
+
+func (a *Argument) Default(value string) *Argument {
+	a.deflt = value
+	a.hasDefault = true
+	return a
+}
+
+func (a *Argument) Required() *Argument {
+	a.required = true
+	return a
+}
+
+func (a *Argument) StringValue() string {
+	return a.value.(*StringValue).Value
+}
+
+func (a *Argument) IntValue() int {
+	return a.value.(*IntValue).Value
+}
+
+func (a *Argument) BoolValue() bool {
+	return a.value.(*BoolValue).Value
 }
